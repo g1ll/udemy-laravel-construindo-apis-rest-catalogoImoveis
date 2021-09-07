d75e2bd07603adb6be9e6804cd2135e54ac3ec38
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-09-06 22:32:38 -0300

    Create migration for manytomany relationship. $>php artisan make:migration create_table_real_state_categories --create=real_state_categories

---
+++ meuImovel/database/migrations/2021_09_07_013202_create_table_real_state_categories.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableRealStateCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('real_state_categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('real_state_categories');
+    }
+}
