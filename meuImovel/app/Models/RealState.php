d624cb47689d15277976633a4b34c8d038121c4d
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-09-08 02:05:47 -0300

    Change RealState model to add fillalble columns.

--- meuImovel/app/Models/RealState.php
+++ meuImovel/app/Models/RealState.php
@@ -8,10 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class RealState extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'content',
+        'price',
+        'bathrooms',
+        'bedrooms',
+        'property_area',
+        'total_property_area',
+        'slug'
+    ];
+
     protected $table = 'real_state';
+
+
     public function user(){
         return $this->belongsTo(User::class); //user_id
     }
-
 }
 
