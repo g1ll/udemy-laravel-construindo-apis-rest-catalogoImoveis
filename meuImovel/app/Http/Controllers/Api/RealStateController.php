54a0da07448add9f231a4e8e0bf60429c9450dee
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-09-08 02:06:31 -0300

    Create save method to register new real_state on DB.

--- meuImovel/app/Http/Controllers/Api/RealStateController.php
+++ meuImovel/app/Http/Controllers/Api/RealStateController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\RealState;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RealStateController extends Controller
 {
     private $realState;
+
     public function __construct(RealState $realState)
     {
         $this->realState = $realState;
@@ -17,5 +19,14 @@ class RealStateController extends Controller
     public function index(){
         $realState = $this->realState->paginate(10);
         return response()->json($realState,200);
+    }
+
+    public function save(Request $request){
+        $data = $request->all();
+
+        if(!$data){
+            return response()->json(['Error'=>'Invalid data sent!'],400);
+        }
+        return response()->json(Auth::user()->real_state()->create($data),201);
     }
 }
