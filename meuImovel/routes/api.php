f56a025c8fd2e591fcf46790d9e1c47497eda7ce
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-09-07 22:06:50 -0300

    Create the route for real-states endpoits.

--- meuImovel/routes/api.php
+++ meuImovel/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->namespace('Api')->group(function (){
+    Route::prefix('real-states')->name('real_states.')->group(function (){
+        Route::get('/',[\App\Http\Controllers\Api\RealStateController::class,'index']); #/api/v1/real-states/
+    });
+});
