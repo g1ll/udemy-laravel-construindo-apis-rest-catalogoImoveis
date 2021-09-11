108b456159416ba9528d97d540ba185d887eedb2
g1ll <gillvelleda@gmail.com>
2021-09-10 22:06:39 -0300

    Using resource to route the endpoints to controller methods.

--- meuImovel/routes/api.php
+++ meuImovel/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\Api\RealStateController;
+use App\Http\Controllers\Api\RealStateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,12 +19,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->namespace('Api')->group(function (){
-    Route::prefix('real-states')->name('real_states')->group(function (){
-        Route::get('/',[RealStateController::class,'index']); #/api/v1/real-states/
-        Route::get('/{real_state}',[RealStateController::class,'show']);
-        Route::post('/',[RealStateController::class,'save'])->middleware('auth.basic'); #/api/v1/real-states/
-        Route::delete('/{real_state}',[RealStateController::class,'remove'])->middleware('auth.basic'); #/api/v1/real-states/id
-        Route::put('/{real_state}',[RealStateController::class,'update'])->middleware('auth.basic'); #/api/v1/real-states/id
+Route::prefix('v1')->group(function (){
+    Route::prefix('real-states')->name('real_states.')->group(function (){
+//        Route::get('/',[RealStateController::class,'index']); #/api/v1/real-states/
+//        Route::get('/{real_state}',[RealStateController::class,'show']);
+//        Route::post('/',[RealStateController::class,'save'])->middleware('auth.basic'); #/api/v1/real-states/
+//        Route::delete('/{real_state}',[RealStateController::class,'remove'])->middleware('auth.basic'); #/api/v1/real-states/id
+//        Route::put('/{real_state}',[RealStateController::class,'update'])->middleware('auth.basic'); #/api/v1/real-states/id
+        Route::resource('/',RealStateController::class);
+
+
     });
 });
