b79a53bf3c9631805532a4feead1b1688d2635a7
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-09-13 22:49:18 -0300

    Create routes for users endpoints.

--- meuImovel/routes/api.php
+++ meuImovel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RealStateController;
@@ -27,5 +28,9 @@ Route::prefix('v1')->group(function (){
 //        Route::delete('/{real_state}',[RealStateController::class,'remove'])->middleware('auth.basic'); #/api/v1/real-states/id
 //        Route::put('/{real_state}',[RealStateController::class,'update'])->middleware('auth.basic'); #/api/v1/real-states/id
         Route::apiResource('real-states',RealStateController::class);
+    });
+
+    Route::name('users.')->group(function (){
+        Route::apiResource('users',UserController::class);
     });
 });
