6f73f9dc6c5f598ae1d925ba908054fdca9a47f6
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-09-14 21:44:56 -0300

    implements CategoryController and adapto to new laravel version.

--- meuImovel/app/Http/Controllers/Api/CategoryController.php
+++ meuImovel/app/Http/Controllers/Api/CategoryController.php
@@ -2,41 +2,82 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Api\ApiMessages;
+
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
     /**
+     * @var Category
+     */
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        //
+        $category = $this->category->paginate('10');
+
+        return response()->json($category, 200);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        try{
+
+            $category = $this->category->create($data);
+
+            return response()->json([
+                'data' => [
+                    'msg' => 'Categoria cadastrada com sucesso!'
+                ]
+            ], 200);
+
+        } catch (\Exception $e) {
+            $message = new ApiMessages($e->getMessage());
+            return response()->json($message->getMessage(), 401);
+        }
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
-        //
+        try{
+
+            $category = $this->category->findOrFail($id);
+
+            return response()->json([
+                'data' => $category
+            ], 200);
+
+        } catch (\Exception $e) {
+            $message = new ApiMessages($e->getMessage());
+            return response()->json($message->getMessage(), 401);
+        }
     }
 
     /**
@@ -44,21 +85,51 @@ class CategoryController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = $request->all();
+
+        try{
+
+            $category = $this->category->findOrFail($id);
+            $category->update($data);
+
+            return response()->json([
+                'data' => [
+                    'msg' => 'Categoria atualizada com sucesso!'
+                ]
+            ], 200);
+
+        } catch (\Exception $e) {
+            $message = new ApiMessages($e->getMessage());
+            return response()->json($message->getMessage(), 401);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
-        //
+        try{
+
+            $category = $this->category->findOrFail($id);
+            $category->delete();
+
+            return response()->json([
+                'data' => [
+                    'msg' => 'Categoria removida com sucesso!'
+                ]
+            ], 200);
+
+        } catch (\Exception $e) {
+            $message = new ApiMessages($e->getMessage());
+            return response()->json($message->getMessage(), 401);
+        }
     }
 }
