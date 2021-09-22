ad003df9694708d1c97063f4e13f7ba0fdb89bcd
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-09-21 22:13:48 -0300

    Create method setThumb.

--- meuImovel/app/Http/Controllers/Api/RealStatePhotoController.php
+++ meuImovel/app/Http/Controllers/Api/RealStatePhotoController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Api\ApiMessages;
 use App\Http\Controllers\Controller;
 use App\Models\RealStatePhoto;
 use Illuminate\Http\Request;
 
-class RealStatePhotoCon troller extends Controller
+class RealStatePhotoController extends Controller
 {
     private  $realStatePhoto;
 
@@ -15,7 +16,25 @@ class RealStatePhotoCon troller extends Controller
         $this->realStatePhoto = $realStatePhoto;
     }
 
-    public function setThumb($photoId){
+    public function setThumb($photoId, $realStateId){
+        try{
+            $this->realStatePhoto
+                ->where('real_state_id',$realStateId)
+                ->where('is_thumb',true)->first();
+
+            $photo = $this->realStatePhoto->find($photoId);
+
+            if($photo->count())$photo->update(['is_thumb'=>false]);
+
+            $photo = $this->realStatePhoto->find($photoId);
+            $photo->update(['is_thumb' => true]);
+
+            return response()->json(['msg'=>'Thumb atualizada'],201);
+
+        }catch (\Exception $e){
+            $message = new ApiMessages("An error occurred!",[$e->getMessage()]);
+            return response()->json($message->getMessage(),400);
+        }
 
     }
 
