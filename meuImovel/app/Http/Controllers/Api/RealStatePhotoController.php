2e6f1f4cecc7a316450879f1a0eb6514febc8e1d
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-09-21 22:01:18 -0300

    Add methods of RealStatePhotoController.

--- meuImovel/app/Http/Controllers/Api/RealStatePhotoController.php
+++ meuImovel/app/Http/Controllers/Api/RealStatePhotoController.php
@@ -3,52 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\RealStatePhoto;
 use Illuminate\Http\Request;
 
-class RealStatePhotoController extends Controller
+class RealStatePhotoCon troller extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    private  $realStatePhoto;
+
+    public function __construct(RealStatePhoto $realStatePhoto)
     {
-        //
+        $this->realStatePhoto = $realStatePhoto;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    public function setThumb($photoId){
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
     }
 
     /**
