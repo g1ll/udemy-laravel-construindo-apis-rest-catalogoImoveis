bb52a952b86614c02c45006133890376d4dd5e63
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-09-27 23:06:51 -0300

    Implements method getJWTCustomClaims from JWTSubject.

--- meuImovel/app/Models/User.php
+++ meuImovel/app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->getKey();
     }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
