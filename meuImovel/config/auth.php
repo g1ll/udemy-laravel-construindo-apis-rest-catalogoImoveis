5f2b4540b7b0322426ec2996b82abefe26504673
Gill from D3567 <velledagonzales@gmail.com>
2021-10-04 23:01:13 -0300

    Configure auth guard.

--- meuImovel/config/auth.php
+++ meuImovel/config/auth.php
@@ -41,6 +41,10 @@ return [
             'driver' => 'jwt',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver'=>'jwt',
+            'provider' => 'users'
+        ]
     ],
 
     /*
