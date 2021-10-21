9500cc32aa52831480742cfd4740c59e5281c622
g1ll from d3567 <gillvelleda@gmail.com>
2021-10-21 20:34:23 -0300

    Fix error: wrong name for table countries.

--- meuImovel/database/migrations/2021_10_21_230744_create_tables_countries_states_cities.php
+++ meuImovel/database/migrations/2021_10_21_230744_create_tables_countries_states_cities.php
@@ -13,7 +13,7 @@ class CreateTablesCountriesStatesCities extends Migration
      */
     public function up()
     {
-        Schema::create('contries', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('slug');
