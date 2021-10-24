e26e40525f6af0149e0dd7a3cf4d0ca5c91475e6
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-10-23 21:32:22 -0300

    Create fields for adresses migration and add foreign keys also.

--- meuImovel/database/migrations/2021_10_21_231441_create_table_adresses.php
+++ meuImovel/database/migrations/2021_10_21_231441_create_table_adresses.php
@@ -15,7 +15,15 @@ class CreateTableAdresses extends Migration
     {
         Schema::create('adresses', function (Blueprint $table) {
             $table->id();
+            $table->string('adress');
+            $table->unsignedBigInteger('state_id');
+            $table->foreignId('city_id')->constrained('cities');
+            $table->string('number');
+            $table->string('neighborhood');
+            $table->string('complement')->nullable();
+            $table->integer('zip_code');
             $table->timestamps();
+            $table->foreign('state_id')->references('id')->on('states');
         });
     }
 
