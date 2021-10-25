25a2cb7512f888e0ec718f5820b5ae80f93534f3
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-10-24 21:19:46 -0300

    Added the relations with Eloquent models

--- meuImovel/app/Models/State.php
+++ meuImovel/app/Models/State.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class State extends Model
 {
     use HasFactory;
+
+    public function country(){
+        return $this->belongsTo(Country::class);
+    }
+
+    public function cities(){
+        return $this->hasMany(City::class);
+    }
+
+    public function adresses(){
+        return $this->hasMany(Adress::class);
+    }
 }
