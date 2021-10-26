729e6dc86c78e1a1e3d1274695b2e0b9be46535e
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-10-25 21:28:28 -0300

    Create methods for RealStateSearch contrller.

--- meuImovel/app/Http/Controllers/Api/RealStateSearchController.php
+++ meuImovel/app/Http/Controllers/Api/RealStateSearchController.php
@@ -7,57 +7,21 @@ use Illuminate\Http\Request;
 
 class RealStateSearchController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private $realState;
+    public function __construct(RealState $realState){
+        $this->realState = $realState;
+    }
+
     public function index()
     {
-        //
+        $realState = $this->realState->paginate(10);
+        return response()->json([
+            'data' => $realState
+        ],200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
     {
         //
     }
