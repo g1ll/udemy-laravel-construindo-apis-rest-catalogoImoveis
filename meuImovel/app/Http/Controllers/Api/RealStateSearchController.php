952fbaad63b1c0f88efabbf208666e2cd16b726e
Gill Velleda Gonzaleclear <gillvelleda@gmail.com>
2021-10-25 21:30:51 -0300

    Fix script error import class RealState model.

--- meuImovel/app/Http/Controllers/Api/RealStateSearchController.php
+++ meuImovel/app/Http/Controllers/Api/RealStateSearchController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\RealState;
 
 class RealStateSearchController extends Controller
 {
