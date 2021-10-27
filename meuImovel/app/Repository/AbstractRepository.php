315b8aaa5f72edf419db526366c6d62ba85ddb13
g1ll from d3567 <gillvelleda@gmail.com>
2021-10-26 23:18:21 -0300

    Created AbstractRepository class to work with filters.

---
+++ meuImovel/app/Repository/AbstractRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repository;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AbstractRepository
+{
+
+    /**
+     * @var Model
+     */
+    private $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function selectFilter($fields){
+        $this->model = $this->model->addSelect(explode(',',$fields));
+    }
+
+    public function addConditions($conditions){
+        $conditions = explode(';',$conditions);
+        foreach ($conditions as $expression) {
+            $exp = explode(':', $expression);
+            $this->model = $this->model->where($exp[0], $exp[1], $exp[2]);
+        }
+    }
+
+    public function getResult(){
+        return $this->model;
+    }
+}
